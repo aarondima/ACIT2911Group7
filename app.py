985e9bafc72fa50311605fcb94928be45b6facc5
Alfred <alfredchan0919@hotmail.com>
2024-05-09 00:48:37 -0700

    left old code as comments, completed bootstrap and basic elements of registration page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from models import Student
 from db import db
 app = Flask(__name__)
@@ -9,10 +10,9 @@ app.config["SECRET_KEY"] = "abc"
 
  
 login_manager = LoginManager()
+login_manager.login_view = 'login'
 login_manager.init_app(app)
- 
- 
- 
+  
 db.init_app(app)
  
  
@@ -24,11 +24,7 @@ def loader_user(user_id):
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        student = Student(username=request.form.get("username"),
-                       firstName=request.form.get("firstName"),
-                       lastName=request.form.get("lastName"),
-                       email=request.form.get("email"),
-                     password=request.form.get("password"))
+        student = Student(username=request.form.get("username"), firstName=request.form.get("firstName"), lastName=request.form.get("lastName"), email=request.form.get("email"), password=request.form.get("password"))
         db.session.add(student)
         db.session.commit()
         return redirect(url_for("login"))
@@ -38,22 +34,26 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        student = Student.query.filter_by(
-            username=request.form.get("username")).first()
+        username=request.form.get("username")
+        password=request.form.get('password')
+        student = Student.query.filter_by(username=username).first()
         print(student)
-        if student.password == request.form.get("password"):
+        
+        if student.password == password:
             login_user(student)
             return redirect(url_for("home"))
     return render_template("login.html")
  
  
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
  
  
 @app.route("/")
+@login_required
 def home():
     return render_template("home.html")
  
