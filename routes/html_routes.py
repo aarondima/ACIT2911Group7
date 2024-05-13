82fbbbc7afa1c6940d462be982611f6aec7c2a77
Sabbasnia <Sabbasnia@my.bcit.ca>
2024-05-13 14:29:17 -0700

    add login

--- routes/html_routes.py
+++ routes/html_routes.py
@@ -4,7 +4,7 @@ from sqlalchemy import delete
 from datetime import datetime
 from db import db
 import csv
-from models import Student
+from models import Student, Course, Instructor
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from flask_login import login_required, current_user
@@ -12,14 +12,28 @@ from flask_login import login_required, current_user
 
 html_bp = Blueprint("html", __name__)
 
+
+@html_bp.route("/home")
 @html_bp.route("/")
 @login_required
 def home():
-    return render_template("base.html")
+    return render_template("home.html",name=current_user.name)
 
 @html_bp.route("/courses")
 def show_courses():
-    return render_template("courses.html")
+    data=db.select(Course).order_by(Course.name).where(Instructor.id == Course.instructor_id)
+    result= db.session.execute(data).scalars()
+    return render_template("courses.html",ins_courses=result)
+
+@html_bp.route("/feedback/", methods=["POST", "GET"])
+def show_feedback():
+    data=db.select(Course).order_by(Course.name)
+    result= db.session.execute(data).scalars()
+    result= db.session.execute(data).scalars()
+    if request.method == "POST":
+        return "<h1>for feedback submitted</h1>"
+    elif request.method == "GET":
+        return render_template("feedback.html",courses=result)
 
 @html_bp.route("/students")
 def show_students():
@@ -39,6 +53,8 @@ def signup():
     phone = request.form["phone"]
     email = request.form["email"]
     password = request.form["password"]
+    if Student.query.filter_by(email=email).first():
+        return redirect(url_for("html.register_page"))
     student = Student(name=name, phone=phone, email=email, password=generate_password_hash(password, method='sha256'),balance=0.0)
     db.session.add(student)
     db.session.commit()
@@ -53,6 +69,7 @@ def logout():
 
 @html_bp.route("/login", methods=["POST"])
 def login():
+    
     email = request.form["email"]
     password = request.form["password"]
     student = Student.query.filter_by(email=email).first()
@@ -61,3 +78,4 @@ def login():
         return redirect(url_for("html.home"))
     else:
         return redirect(url_for("html.register_page"))
+    
